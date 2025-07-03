7b43e62055b1e1a70cc90c827c8fdb3d91dad0a9
Sourav <souravbhardwaj997@gmail.com>
2025-07-03 21:46:16 +0530

    Enhance About section with GSAP animations and improve grid layout in CSS

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,10 +1,49 @@
+import { useGSAP } from "@gsap/react";
+import gsap from "gsap";
+import { SplitText } from "gsap/all";
+
 const About = () => {
+  useGSAP(() => {
+    const t1 = gsap.timeline({
+      scrollTrigger: {
+        trigger: "#about",
+        start: "top center",
+      },
+    });
+    const heading = new SplitText(".heading", {
+      type: "words",
+    });
+
+    t1.from(heading.words, {
+      y: 80,
+      opacity: 0,
+      stagger: 0.02,
+    });
+
+    t1.from(".top-grid", {
+      y: 30,
+      opacity: 0,
+      stagger: 0.02,
+    });
+
+    t1.from(
+      ".bottom-grid",
+      {
+        y: 30,
+        opacity: 0,
+        stagger: 0.02,
+      },
+      "-=0.5"
+    );
+  });
   return (
     <section id="about">
       <div className="content">
-        <div className="col-span-5">
+        <div className="md:col-span-5">
           <div className="badge">Best Cocktails</div>
-          <h2>Where every detail matters—from muddle to garnish</h2>
+          <h2 className="heading">
+            Where every detail matters—from muddle to garnish
+          </h2>
         </div>
         <div className="col-span-3"></div>
         <div className="sub-content">
@@ -15,9 +54,11 @@ const About = () => {
           </p>
           <div className="grid grid-cols-2 items-center">
             <div className="border-r border-gray-800">
-              <span className="flex gap- text-white">
+              <span className="flex text-white">
                 {[1, 2, 3, 4, 5].map((el) => (
-                  <p className="text-white">⭐</p>
+                  <p className="text-white" key={el}>
+                    ⭐
+                  </p>
                 ))}
               </span>
               <p>
@@ -27,27 +68,55 @@ const About = () => {
             </div>
             <div className="relative h-20">
               <div className="rounded-full p-3 noisy">
-                {/* <div className="flex flex-row">
+                <div className=" mx-auto flex flex-row gap-0 items-center h-full w-fit">
                   <img
                     src="/images/profile1.png"
-                    className="rounded-full h-10 w-10"
+                    className="rounded-full h-10 w-10 border-2 border-white"
                   />
                   <img
                     src="/images/profile2.png"
-                    className="rounded-full h-10 w-10"
+                    className="rounded-full h-10 w-10 border-2 border-white"
                   />
                   <img
                     src="/images/profile3.png"
-                    className="rounded-full h-10 w-10"
+                    className="rounded-full h-10 w-10 border-2 border-white"
                   />
                   <img
                     src="/images/profile4.png"
-                    className="rounded-full h-10 w-10"
+                    className="rounded-full h-10 w-10 border-2 border-white"
                   />
-                </div> */}
+                </div>
               </div>
             </div>
           </div>
+        </div>
+      </div>
+      <div className="top-grid mt-16">
+        <div className="col-span-1 md:col-span-1 xl:col-span-3">
+          <div className="noisy" />
+          <img src="/images/abt1.png" />
+        </div>
+        <div className="col-span-1 md:col-span-1 xl:col-span-3">
+          <div className="noisy" />
+
+          <img src="/images/abt2.png" />
+        </div>
+        <div className="col-span-1 md:col-span-2 xl:col-span-6">
+          <div className="noisy" />
+
+          <img src="/images/abt3.png" />
+        </div>
+      </div>
+      <div className="bottom-grid">
+        <div className="col-span-1 md:col-span-1 xl:col-span-8">
+          <div className="noisy" />
+
+          <img src="/images/abt5.png" />
+        </div>
+        <div className=" col-span-1 md:col-span-1 xl:col-span-4">
+          <div className="noisy" />
+
+          <img src="/images/abt4.png" />
         </div>
       </div>
     </section>
